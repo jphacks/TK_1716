4bd42a7923d4418b9575117108853d631581ebbf
Hosei <tomoyuki.yamasaki@gmail.com>
2017-10-28 12:58:03 +0000

    raspi to tougou

--- raspi/myserial.py
+++ raspi/myserial.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import serial
-#ser = serial.Serial('/dev/ttyACM0', 9600)
-ser = serial.Serial('/dev/cu.usbmodem1421', 9600)
+ser = serial.Serial('/dev/ttyACM0', 9600)
+#ser = serial.Serial('/dev/cu.usbmodem1421', 9600)
 s = [0,1]
 while True:
     read_serial = ser.readline()
